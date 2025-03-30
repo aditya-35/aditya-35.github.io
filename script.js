221885d8f2fa3962b2368f1ab9ba38c46fbac351
aditya-35 <knpaditya2001@gmail.com>
2025-03-30 22:52:17 +0530

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 console.log("Hello World ! Aditya");
-function myArray(num1, num2) {
-  return num1 + num2;
-}
-const Loop = myArray(2, 3);
-console.log(myArray);
-// console.log();
+const Resume = document.querySelector(".btn");
+Resume.addEventListener("click", () => {
+  alert("Resume Downloaded");
+});
